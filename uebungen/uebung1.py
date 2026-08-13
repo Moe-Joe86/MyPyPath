9329f69825b483eff28197d01d52d02df05fdc86
MoeJoe <patrick.wels@web.de>
2026-08-13 13:19:22 +0200

    fertig? versuch1

--- uebungen/uebung1.py
+++ uebungen/uebung1.py
@@ -1,11 +1,8 @@
-geburtsjahr = None
-geburtsjahr_int = (int(geburtsjahr))
 jahr_aktuell = 2026
-alter = jahr_aktuell - geburtsjahr_int
+geburtsjahr = input("Wann wurdest du geboren?")
+geburtsjahr_int = int(geburtsjahr)
 
+print("du bist", jahr_aktuell - geburtsjahr_int)
 
-print("Wann wurdest du geboren?")
-geburtsjahr = input()
-print(jahr_aktuell - geburtsjahr)
 
 
