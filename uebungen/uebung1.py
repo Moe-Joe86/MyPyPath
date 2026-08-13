3e6e61b5e6d0bb5f9f5c9d183b1a9d636d96ba76
Moe-Joe86 <patrick.wels@web.de>
2026-08-13 12:19:38 +0200

    uebung fix

--- uebungen/uebung1.py
+++ uebungen/uebung1.py
@@ -1,7 +1,7 @@
-geburtsjahr = none
-geburtsjahr_int = (int(alter))
+geburtsjahr = None
+geburtsjahr_int = (int(geburtsjahr))
 jahr_aktuell = 2026
-
+alter = jahr_aktuell - geburtsjahr_int
 
 
 print("Wann wurdest du geboren?")
