12ed5310560ceeb23d12fea24b9a0e0a62dac3c9
MoeJoe <patrick.wels@web.de>
2026-09-15 14:09:53 +0200

    et6 3/4

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import time
 import random
 
-player_class = None
 player_name = None
+player_class = None
+KLASSEN = ("soldat", "heavy", "engineer", "medic")
+
 
 #equipment
 rapid_deployer = None        #für engineer kurz RD-4
@@ -15,21 +17,19 @@ machinegun = None
 health = 100
 damage = 10
 armor = 5
-exp = 99
+exp = 0
 kern_integritaet = 100
 
 #resouces
 ammo_max = 40
 core_max = 100
-magazin = 30   #Menge an Munition pro Magazin
 geladen = 5
 magazin_groesse = 5
+munitionskiste = 30
 
 inventory = []
 max_inventory = 10
 recruts = 0
-medkit = None
-reparaturkit = None
 
 balken_laenge = 10
 
@@ -37,26 +37,57 @@ balken_laenge = 10
 wave_to_evacuation = 20
 enemy = []
 reload_necessary = False
-enemy_in_sight = None
+enemy_in_sight = True
 radiomessage_transmitted = None
 answere = None
+bekannte_gegnertypen = set()
+freigeschaltet = set()
+
+
 
 #------------------------------------------------------------------------------------------------------
 #DAS DEPOT / VORRAT
 
-waren = {"medkit": 25, "magazin": 15, "panzerplatte": 90}   #Preisangabe, keine Menge!
-verkaufswerte = {"chitinpanzer": 3, "organ": 5, "saeuredruese": 8}
+waren = {"medkit": 25, "munitionskiste": 15, "panzerplatte": 90, "reparaturkit": 60}   #Preisangabe, keine Menge!
+stapelbar = {"medkit": False, "munitionskiste": True, "panzerplatte": False, "reparaturkit": False}   #Wird nur für Waren und Depot benötigt. 
 
-stapelbar = {"medkit": False, "magazin": True, "panzerplatte": False, "munition": True, "chitinpanzer": True, "organ": True, "saeuredruese": True}   #Wird nur für Waren und Depot benötigt. 
+UPGRADES = {"zielhilfe": 60, "grossmagazin": 80, "schnellfeuer": 120} #Preisangabe, keine Menge!
+
+verkaufswerte = {"chitinpanzer": 3, "organ": 5, "saeuredruese": 8}
 
 vorrat = {"vaporium": 100, "munition": 40, "chitinpanzer": 2, "organ": 1, "saeuredruese": 0} #Mengenangabe!!!
 
-anzeigenamen = {"medkit": "MedKit", "munition": "Munition", "panzerplatte": "Panzerplatte", "organ": "Organ",
-    "vaporium": "Vaporium", "chitinpanzer": "Chitinpanzer", "saeuredruese": "Säuredrüse", "magazin": "Magazin(30 Schuss)"}
+GEGNERTYPEN = {"kriecher":
+                {
+                "kurz": "Der Kriecher tritt fast ausschließlich in riesigen Schwärmen auf, um den Feind blitzschnell zu überrennen.", 
+                "lang": "Der Kriecher wurde vom der Brut zu einer perfektionierten Tötungsmaschine mutiert. Von der Größe eines großen Hundes, greift er mit messerscharfen Sensenklauen und kraftvollen Beißwerkzeugen an. Ihre wahre Stärke liegt in ihrer Masse und ihrer enormen Fortbewegungsgeschwindigkeit. Einzeln sind sie leicht zu eliminieren, doch im Kollektiv fluten sie die Schlachtfelder, umgehen Verteidigungslinien und überrennen selbst stark befestigte Stellungen in Sekundenschnelle durch ihre schiere Überzahl.",
+                "zeichen": "K"
+                },
+            "speier":
+                {
+                "kurz": "Der Speier ist die vielseitige Fernkampfeinheit der Brut.", 
+                "lang": "Diese berüchtigte Variante der Brut wurde von Veteranen bereits seit den ersten intergalaktischen Konflikten als „Speier“ bezeichnet. Er ist in der Lage dichte Salven agressiver Säure über mittlere Distanzen auf Boden- und Luftziele zu verspritzen. Die Substanz verätzt organische Materie in Sekundenschnelle und ist in der Lage, selbst die hochentwickelte Neostahl-Panzerung terranischer Marines in kurzer Zeit zu zersetzen.",
+                "zeichen": "S"
+                },
+            "panzerbrut":
+                {
+                "kurz": "Die Panzerbrut ist ein wandelnder Festungsorganismus aus nahezu unzerstörbarem Chitin.", 
+                "lang": "Die Panzerbrut ist ein biologisches Meisterwerk der defensiven Evolution. Sie ist darauf ausgelegt ist, jede Agression zu annihilieren. Ihr gesamter Körper besteht aus einer zentimeterdicken, wellenartig geschichteten Knochenpanzerung, die durch die Bildung seltener Oberflächenkristalle eine extreme Dichte erreicht hat. Gewöhnliche Projektile prallen meist wirkungslos von den Oberflächen ab. Lediglich panzerbrechende HEAT-Munition zeigt eine Wirkung, indem sie sich typischerweise beim Aufprall in eine verflüssigte Kupfernadel verwandelt, welche mit Mach 30 den Panzer durchdringt und die Eingeweide zerreißt.",
+                "zeichen": "P"
+                }
+            }
+
+anzeigenamen = {"medkit": "MedKit", "munitionskiste": "Munitionskiste(30 Schuss)", "panzerplatte": "Panzerplatte", "organ": "Organ",
+    "vaporium": "Vaporium", "chitinpanzer": "Chitinpanzer", "saeuredruese": "Säuredrüse", "datenkern": "Datenkern", "reparaturkit": "Reparaturkit",
+    "medic": "Medic", "heavy": "Heavy", "soldat": "Soldat", "engineer": "Engineer", "zielhilfe": "Zielhilfe", "grossmagazin": "Großmagazin", "schnellfeuer": "Schnellfeuer",
+    "panzerbrut": "Panzerbrut", "speier": "Speier", "kiecher": "Kriecher"}
 
 stufenaufstieg = {"stufe1": 100, "stufe2": 300, "stufe3": 900, "stufe4": 3000, "stufe5": 10000}
 stufe = 0
         
+inventory = []
+loot_table = ["chitinpanzer", "organ", "saeuredruese", "datenkern"]
+loot = []
 
 
 
@@ -92,7 +123,7 @@ sectors = {
         "beschreibung": "Du stehst am östlichen Tor. Der Weg ist verschüttet. Dahinter liegt die Landeplattform.", "integritaet": 100, "nachbarn": {
             "westen": "kern"}},
     "kern":{
-        "beschreibung": "Die zentrale Bunkeranlage des Außenpostens.", "integritaet": 100, "nachbarn": {
+        "beschreibung": "Die zentrale Bunkeranlage des Außenpostens.", "nachbarn": {
             "sueden": "werkstatt", "norden": "nordtor", "osten": "osttor", "westen": "depot"}},
     "depot":{
         "beschreibung": "Hier kann man Gegenstände kaufen.", "integritaet": 100, "nachbarn": {
@@ -106,7 +137,7 @@ sectors = {
 
 }
 
-aktueller_sektor = sectors["nordtor"]
+aktueller_sektor = sectors["depot"]
 
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -137,25 +168,24 @@ print()
 time.sleep(1)
 print()
 
+#-------------------------------------------------------------------------------------------------------------------
+#Klassenauswahl
+
 print("Rekrut, welches spezialisierte Training haben Sie durchlaufen?")
-print()
-print("-Soldier")
-print("-Medic")
-print("-Heavy")
-print("-Engineer")
-print()
+
+for klassenwahl in KLASSEN:
+    print(anzeigenamen.get(klassenwahl, klassenwahl))
+
 player_class = input("Wähle eine Klasse und gebe den Namen ein:  ")
 player_class = player_class.lower().strip()
 
-if player_class != "soldier" and player_class != "medic" and player_class != "heavy" and player_class != "engineer": # hiermit möchte ich Auftrag 3 ohne else erfüllen. Diese Bedingung kann ich auch mit == und einem else erfüllen, ist aber kostspieliger. bewusste entscheidung gegen die Aufgabe!
+if player_class not in KLASSEN:
     player_class = input("Falsche Eingabe. Gib Soldier, Medic, Heavy oder Engineer ein: ")
     player_class = player_class.lower().strip()
-
-if player_class == "soldier":
+elif player_class == "soldat":
     health = 100
     damage = 10
     armor = 5
-
     machinegun = True
 elif player_class == "medic":
     health = int(health * 0.8)
@@ -218,60 +248,85 @@ while repeat:
 
 """
 
-enemy_in_sight = True
+
 print()
 
 print(ascii_kopf)
 
-inventory = []
-loot_table = ["chitinpanzer", "organ", "saeuredruese", "datenkern"]
-loot = []
-
 #--------------------------------------------------------------------------------------------------------------------
-#WELLEN UND RUNDEN INITIALISIEREN
-#GEGNER SPAWNEN
+#--------------------------------------------------------------------------------------------------------------------
 
+#Start der Wellen
 for wave in range(1, wave_to_evacuation + 1):
-    print(f"--- Welle {wave} von {wave_to_evacuation} ---")
 
-    
     round_nr = 1
-    enemy = []
+    gegner_pos = []
+    spawnende_gegnertypen = []
+    moegliche_gegnertypen = set()
+
+    if "grossmagazin" in freigeschaltet:
+        magazin_groesse = round(magazin_groesse * 1.5)    
+
+
+#SPAWN    
+    if wave >= 1:
+        moegliche_gegnertypen.add("kriecher")
+    if wave > 3 and wave < 8:
+        moegliche_gegnertypen.add("speier")
+    if wave > 7:
+        moegliche_gegnertypen.add("panzerbrut")
+    
+    for info in moegliche_gegnertypen:
+        if info in bekannte_gegnertypen:
+            print(GEGNERTYPEN[info]["kurz"])        
+        else:
+            print(GEGNERTYPEN[info]["lang"])
+            bekannte_gegnertypen.add(info)
 
     for spawn in range(wave):
-        enemy.append(5)
+        if spawn == 0 and "panzerbrut" in moegliche_gegnertypen:
+            gegner_pos.append(10)
+            spawnende_gegnertypen.append("panzerbrut")
+        elif (spawn == 0 and "speier" in moegliche_gegnertypen) or (spawn == 1 and "speier" not in spawnende_gegnertypen):
+            gegner_pos.append(10)
+            spawnende_gegnertypen.append("speier")
+        else:
+            gegner_pos.append(10)
+            spawnende_gegnertypen.append("kriecher")
 
+
+
+#wellenstatus
+    print(f"--- Welle {wave} von {wave_to_evacuation} ---")
     print()    
-    print(f"{len(enemy)} Gegner befinden sich im Anmarsch.")
-    
+    print(f"{len(gegner_pos)} Gegner befinden sich im Anmarsch.")
 
-    while len(enemy) > 0 and (kern_integritaet > 0 and health > 0):
+
+#Start der Runden
+    while len(gegner_pos) > 0 and (kern_integritaet > 0 and health > 0):
         
         stufe = 0
 
         for up in stufenaufstieg:
-            if exp > stufenaufstieg[up]:   
+            if exp >= stufenaufstieg[up]:   
                 stufe += 1
         
         print()
         print(f"Welle {wave} | Runde {round_nr}")
 
-#------------------------------------------------------------------------------------------------------------------------
 # DIE ANMARSCHBAHN
-
-        pos_on_map = ["."] * 5
-        closest_enemy = min(enemy)
+        anmarschbahn = ["."] * 10
+        closest_enemy = min(gegner_pos)
         
-        for enemy_pos in range(len(enemy)):
-            if enemy[enemy_pos] > 0:
-                pos_on_map[-enemy[enemy_pos]] = "k"
+        for anmarsch_pos in range(len(gegner_pos)):     
+            if gegner_pos[anmarsch_pos] > 0:            
+                anmarschbahn[-gegner_pos[anmarsch_pos]] = GEGNERTYPEN[spawnende_gegnertypen[anmarsch_pos]]["zeichen"]
+        
         print("Das Loch @ " + "".join(pos_on_map) + " /-\ Vorposten")
 
 
-#------------------------------------------------------------------------------------------------------------------------
 #AUSFÜHRBARE AKTIONEN       
- 
-        print("Wähle eine der Aktionen:\n--beenden --feuer --status --nachladen --inventar --umsehen --gehe --waren --kaufe --map")
+        print("Wähle eine der Aktionen:\n--beenden --feuer --status --nachladen --inventar --umsehen --gehe --waren --kaufe x --verkaufe --upgrades --upgrade x --map --bestiarium")
         action = input("--").lower().split()   #test,schaden sind entwicklerwerkzeuge
         print()
 
@@ -282,24 +337,34 @@ for wave in range(1, wave_to_evacuation + 1):
             word2 = ""
             if len(action) > 1:
                 word2 = action[1]
-            if word1 == "feuer" or (word1 == "feuer" and word2 == "frei"):
+            if word1 == "feuer" or word1 == "feuern":
                 if reload_necessary:
                     print("Magazin leer.")
-                else:
-                    print("Feuer frei!")
+                else:                
+                    if "schnellfeuer" in freigeschaltet:
+                        gegner_pos.remove(closest_enemy)
+                        exp += 10
+                        geladen -= 1
+                        loot.append(random.choice(loot_table))
+                        if geladen == 0:
+                            print("Magazin ist jetzt leer!")
+                            reload_necessary = True
+                if reload_necessary:
+                    print("Magazin leer.")
+                elif len(gegner_pos) > 0: #muss zwangsweise immer größer 0 sein ohne schnellfeuer
+                    gegner_pos.remove(closest_enemy)
                     geladen -= 1
-                
-                    enemy.remove(closest_enemy)
                     exp += 10
                     round_nr += 1
-                    kern_integritaet -= len(enemy) * 2 
-                    health -= len(enemy) * 1
-                    loot.append(random.choice(loot_table))
+                    loot.append(random.choice(loot_table))  
                     if geladen == 0:
                         print("Das Magazin ist jetzt leer!")
                         reload_necessary = True                        
-                    for move in range(len(enemy)):
-                        enemy[move] -= 1
+                    for move in range(len(gegner_pos)):
+                        gegner_pos[move] -= 1
+
+                kern_integritaet -= len(gegner_pos) * 2
+                health -= len(gegner_pos) * 1        
 
             elif word1 == "nachladen" or (word1 == "lade" and word2 == "nach") :
                 round_nr += 1
@@ -315,10 +380,10 @@ for wave in range(1, wave_to_evacuation + 1):
                     print("Nachgeladen.")
 
 
-                kern_integritaet -= len(enemy) * 2
+                kern_integritaet -= len(gegner_pos) * 2
                 reload_necessary = False                  
-                for move in range(len(enemy)):
-                    enemy[move] -= 1
+                for move in range(len(gegner_pos)):
+                    gegner_pos[move] -= 1
 
             elif word1 == "beenden" or (word1 == "welle" and word2 == "beenden"):
                 print(f"Welle {wave} beendet")
@@ -373,7 +438,7 @@ for wave in range(1, wave_to_evacuation + 1):
                 if len(action) == 1:
                     print("Keine Richtung ausgewählt. Nutze --umsehen, um Richtungen zu sehen.")
                 else:
-                    if len(action) == 2 and word2 in sectors:
+                    if len(action) == 2 and word2 in sectors[aktueller_sektor]["nachbarn"]:
                         print(f"Ich gehe zum Sektor {word2}.")
                         aktueller_sektor = sectors[word2] #vergeht eine Runde?
                     else:
@@ -406,12 +471,12 @@ for wave in range(1, wave_to_evacuation + 1):
                             print(f"{anzeigenamen.get(word2, word2)} erfolgreich gekauft.")   
                         else:
                             anzahl = int(input(f"Wieviele möchtest du kaufen? "))
-                            if anzahl < 0:
-                                anzahl *= -1
+                            if anzahl < 1:
+                                print("Ungültige Eingabe.")
                             if vorrat["vaporium"] < (waren[word2] * anzahl):
                                 print("Du besitzt nicht genügend Vaporium.")
-                            else:
-                                vorrat["munition"] += (magazin * anzahl)
+                            else:                                                       # aktuell NUR MUNITION ALS STAPELBAR!!!!!
+                                vorrat["munition"] += (munitionskiste * anzahl)
                                 vorrat["vaporium"] -= (waren[word2] * anzahl)
                                 print(f"{anzahl} {anzeigenamen.get(word2, word2)} erfolgreich gekauft.")                
             
@@ -427,10 +492,49 @@ for wave in range(1, wave_to_evacuation + 1):
                     vorrat["vaporium"] += bezahlung    
                     print()
                     print(f"Du hast insgesamt {bezahlung} Vaporium erhalten.")
-                    print(f"Du besitzt jetzt {vorrat['vaporium']} Vaporium.")        
+                    print(f"Du besitzt jetzt {vorrat['vaporium']} Vaporium.") 
 
+            elif word1 == "upgrades":
+                if aktueller_sektor != sectors["depot"]:
+                    print("Gehe zum Depot. Hier gibt es keine Upgrades.")
+                for verbesserung in UPGRADES:
+                    if verbesserung not in freigeschaltet:
+                        print(f"{anzeigenamen.get(verbesserung, verbesserung)}: {UPGRADES[verbesserung]} Vaporium")
+                    else:
+                        print(f"{anzeigenamen.get(verbesserung, verbesserung)}: bereits erworben.") 
+
+            elif word1 == "upgrade":   
+                if aktueller_sektor != sectors["depot"]:
+                    print("Gehe zum Depot. Hier kann man nichts kaufen.")
+                elif len(action) == 1:
+                    print("Kein Upgrade ausgewählt. Gebe --upgrades ein, um die Upgrades zu sehen.")                    
+                elif word2 not in UPGRADES:
+                    print("Dieses Upgrade ist nicht verfügbar.")
+                elif vorrat["vaporium"] < UPGRADES[word2]:
+                    print("Du besitzt nicht genügend Vaporium.")
+                elif word2 in freigeschaltet:
+                    print("Bereits erworben.")
+                else:
+                    vorrat["vaporium"] -= UPGRADES[word2]
+                    freigeschaltet.add(word2)
+                    print(f"{word2} erworben.")                    
+                        
             elif word1 == "map":
                 print(karte)
+            
+            elif word1 == "bestiarium" and len(word2) == 0:
+                for gegnerauflistung in bekannte_gegnertypen:
+                    print(f"{anzeigenamen.get(gegnerauflistung, gegnerauflistung)}: {GEGNERTYPEN[gegnerauflistung]["kurz"]}")
+                print(f"Du hast {len(bekannte_gegnertypen)} von {len(GEGNERTYPEN)} entdeckt.")
+
+            elif word1 == "bestiarium" and len(word2) > 0:
+                if word2 not in GEGNERTYPEN:
+                    print("Diesen Gegner gibt es nicht.")
+                elif word2 not in bekannte_gegnertypen and word2 in GEGNERTYPEN:
+                    print("Zu diesem Gegner konnten unsere Marines noch keine Informationen sammeln.")
+                elif word2 in bekannte_gegnertypen and word2 in GEGNERTYPEN:
+                    print(GEGNERTYPEN[word2]["lang"])
+                                
 
             elif word1 == "status" or (word1 == "status" and  word2 == "anzeigen"):
                 """
@@ -448,7 +552,7 @@ for wave in range(1, wave_to_evacuation + 1):
                 print(f"Ammo: {vorrat['munition']}")
                 print(f"Schaden: {damage}")
                 print(f"Rekruten: {recruts}")
-                print(f"Gegner: {enemy}")
+                print(f"Gegner: {gegner_pos}")
                 print(f"Erfahrung: {exp}")
                 print(f"Stufe: {stufe}")
                 print(f"Nachladen nötig: {reload_necessary}")
@@ -458,13 +562,16 @@ for wave in range(1, wave_to_evacuation + 1):
                 print("Kern      [" + ("#" * kern_balken) + ("·" * kern_rest) + "]")
                 print("Health    [" + ("#" * health_balken) + ("·" * health_rest) + "]")
                 print("Munition  [" + ("#" * ammo_balken) + ("·" * ammo_rest) + "]")"""
+    
 
 
     if kern_integritaet <= 0:        
-        break
         print("Deine Basis wurde zerstört.")
-    elif health <= 0:
         break
+        
+    elif health <= 0:
         print("Du bist gestorben.")
+        break
+        
         
 
--- kladde.py
+++ kladde.py
@@ -1,1 +1,20 @@
-print(["."] * 8)
+GEGNERTYPEN = {"kriecher":
+                {
+                "kurz": "Der Kriecher tritt fast ausschließlich in riesigen Schwärmen auf, um den Feind blitzschnell zu überrennen.", 
+                "lang": "Der Kriecher wurde vom der Brut zu einer perfektionierten Tötungsmaschine mutiert. Von der Größe eines großen Hundes, greift er mit messerscharfen Sensenklauen und kraftvollen Beißwerkzeugen an. Ihre wahre Stärke liegt in ihrer Masse und ihrer enormen Fortbewegungsgeschwindigkeit. Einzeln sind sie leicht zu eliminieren, doch im Kollektiv fluten sie die Schlachtfelder, umgehen Verteidigungslinien und überrennen selbst stark befestigte Stellungen in Sekundenschnelle durch ihre schiere Überzahl.",
+                "zeichen": "K"
+                },
+            "speier":
+                {
+                "kurz": "Der Speier ist die vielseitige Fernkampfeinheit der Brut.", 
+                "lang": "Diese berüchtigte Variante der Brut wurde von Veteranen bereits seit den ersten intergalaktischen Konflikten als „Speier“ bezeichnet. Er ist in der Lage dichte Salven agressiver Säure über mittlere Distanzen auf Boden- und Luftziele zu verspritzen. Die Substanz verätzt organische Materie in Sekundenschnelle und ist in der Lage, selbst die hochentwickelte Neostahl-Panzerung terranischer Marines in kurzer Zeit zu zersetzen.",
+                "zeichen": "S"
+                },
+            "panzerbrut":
+                {
+                "kurz": "Die Panzerbrut ist ein wandelnder Festungsorganismus aus nahezu unzerstörbarem Chitin.", 
+                "lang": "Die Panzerbrut ist ein biologisches Meisterwerk der defensiven Evolution. Sie ist darauf ausgelegt ist, jede Agression zu annihilieren. Ihr gesamter Körper besteht aus einer zentimeterdicken, wellenartig geschichteten Knochenpanzerung, die durch die Bildung seltener Oberflächenkristalle eine extreme Dichte erreicht hat. Gewöhnliche Projektile prallen meist wirkungslos von den Oberflächen ab. Lediglich panzerbrechende HEAT-Munition zeigt eine Wirkung, indem sie sich typischerweise beim Aufprall in eine verflüssigte Kupfernadel verwandelt, welche mit Mach 30 den Panzer durchdringt und die Eingeweide zerreißt.",
+                "zeichen": "P"
+                }
+            }
+print(len(GEGNERTYPEN))
